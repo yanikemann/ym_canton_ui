4b9bbb6d6575d505b1c3842405a53ba4be82d553
yanikemann <yanikemann@yeoworks.io>
2025-02-09 12:42:31 -0600

    fixed prop name

--- lib/src/styled_components/canton_expansion_tile.dart
+++ lib/src/styled_components/canton_expansion_tile.dart
@@ -211,7 +211,7 @@ class CantonExpansionTileState extends State<CantonExpansionTile>
     final ThemeData theme = Theme.of(context);
     _borderColorTween.end = theme.dividerColor;
     _headerColorTween
-      ..begin = theme.textTheme.subtitle1!.color
+      ..begin = theme.textTheme.titleMedium!.color
       ..end = theme.colorScheme.primary;
     _iconColorTween
       ..begin = theme.unselectedWidgetColor
